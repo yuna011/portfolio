70cda87149dfc373a15c1b0e159066353ec78ad4
森尾　優菜 <>
2024-03-30 14:01:54 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const mail = document.querySelector(`.fa-envelope`);
 const contact = document.querySelector(`.contactWrap`);
+const closed = document.querySelector(`.fa-xmark`);
+const modal = document.querySelector(`.modal`);
+
 
 const user = document.querySelector(`.fa-user`);
 const profile = document.querySelector(`.pWrap`);
@@ -7,12 +10,11 @@ const profile = document.querySelector(`.pWrap`);
 mail.onclick = () => {
     contact.style.display = `block`;
 }
+closed.onclick = () => {
+    modal.style.display = `none`;
+}
 
 user.onclick = () => {
     profile.style.display = `block`;
 }
 
-// mail.addEventListener('click', modalOpen);
-// function modalOpen() {
-//     contact.style.display = 'block';
-// }
